4be8b2665553fc0833a324515117d46b5526c7f4
Sherif Abdullah <85506496+sherif-abdallah@users.noreply.github.com>
2021-12-16 01:42:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def main():
     table()
 
     st.write("An overview of both the salaries and technologies in Egypt's tech market")
-    st.write("Introduction")
+    st.write("### Introduction")
     st.write("Egypt's tech market is a bit vague. You can't access information regarding salaries, popular technologies, and companies' environment without finding yourself having to get in contact with employees working inside the company. Famous reviews websites like Glassdoor aren't much of a help since most employees don't disclose their salaries even anonymously and only angry employees decide to write reviews on Glassdoor.")
     st.write("This unintelligibility makes anyone who wants to value themselves in the market fail to do so. A lot of people often ask me: Which technologies should we learn to increase our chances in the market? Or how much should I ask for? And so on. While it's true that some companies are technology agnostic when it comes to hiring where they test you in your Problem Solving and Design skills, most companies, in Egypt, favour hiring someone who is already comfortable with their tech stack.")
     st.write("Disclaimer: This by no means is a comprehensive overview of the market, it's biased toward my network and the companies I'm interested in. Having said that, I assure you that I made an effort trying to clean the data and make it fair. I didn't include anyone working for a remote company, all the data that we analyse here are shared by Software Engineers working in companies operating in Egypt.")
